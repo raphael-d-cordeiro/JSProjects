f68e45d087b638eb68e819efd5505bbbd95c707f
RAPHAEL D CORDEIRO <raphael.d.cordeiro@gmail.com>
2021-06-25 21:13:22 -0300

    ROTAS, HOOKS E AUTH FIREBASE

--- ReactJS/letmeask/src/components/Button.tsx
+++ ReactJS/letmeask/src/components/Button.tsx
@@ -1,22 +1,15 @@
-// type ButtonProps = {
-//   children?: string;
-// }
+import { ButtonHTMLAttributes } from 'react'
 
-// export function Button(props: ButtonProps) {
-//   return (
-//     <button>{props.children || 'Default'}</button>
-//   )
-// }
-import { useState } from "react";
-export function Button() {
-  // let counter = 0;
-  const [counter, setCounter] = useState(0)
-  function increment() {
-    setCounter(counter + 1);
-  }
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps) {
+
   return (
-    <button onClick={increment}>{counter}</button>
+    <button className="button" {...props} />
   )
 }
 
-//named export
+
+
